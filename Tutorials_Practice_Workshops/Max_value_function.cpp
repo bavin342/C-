282252d2ce813a3b131007281b30a06979f734f0
bavin342 <bavsri11@gmail.com>
2022-03-06 18:26:58 +0000

    Creating and updating code for tutorial practice and workshops

---
+++ Tutorials_Practice_Workshops/Max_value_function.cpp
@@ -0,0 +1,28 @@
+//Workshop tutorial questions and practice//
+
+#include <iostream>
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+/* Conditional Operators and switch statements*/
+/* Finding the Max value given alpha and beta*/
+
+
+void max_function(){
+	int alpha = 40, beta = 50;
+	int max = (alpha > beta) ? alpha : beta;
+	cout << max << endl;
+}
+
+int main() {
+	max_function();
+	system("pause");
+	return 0;
+}
+
+
+
+
+
